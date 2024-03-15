d8d3c8c64272df2d155b118f6e58938273fd04e1
Lachlan Stephen <lachlanstephen@outlook.com>
2024-03-16 00:43:39 +1030

    Added Day 5 Docs and stuff

---
+++ week_4_stacks_&_queues/05_call_counter/ft_call_counter.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_call_counter.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lstephen <lstephen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 21:49:30 by lstephen          #+#    #+#             */
+/*   Updated: 2024/03/16 00:42:40 by lstephen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct
+{
+
+
+
+}
+
+int	ft_call_counter(
+
+
+
+int	main(void)
+{
+	return (0);
+}
