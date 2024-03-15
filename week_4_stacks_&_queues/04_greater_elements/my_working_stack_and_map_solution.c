5d4dc9d0c54f9e08d3a44b07e19fffc057311dac
lachlanstephen <lachlanstephen@outlook.com>
2024-03-16 00:18:10 +1030

    Week 4 Day 4 Improved

---
+++ week_4_stacks_&_queues/04_greater_elements/my_working_stack_and_map_solution.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_SIZE 5
+
+int* ft_greater_elements(int* nums1, int nums1Len, int* nums2, int nums2Len) {
+    int map[4];
+  int *res = (int*)malloc(sizeof(int) * nums1Len);
+  if (res == NULL)
+    return NULL;
+  for (int i = 0; i < nums1Len; i++)
+      res[i] = -1;
+
+  int stack[MAX_SIZE];
+  int top = -1;
+
+  for (int i = 0; i < nums2Len; i++) {
+    map[i] = -1;
+  }
+  for (int i = 0; i < nums1Len; i++) {
+    for (int j = 0; j < nums2Len; j++) {
+      if (nums1[i] == nums2[j]) {
+        map[j] = i;
+        break;
+      }
+    }
+  }
+  int count = 0;
+  for (int i = 0; i < nums2Len; i++) {
+    if (top != -1 && nums2[i] > stack[top]) {
+      //int popped = stack[top--];
+      if (i > 0 && map[i - count] != -1) {
+        res[map[i-- - count]] = nums2[i];
+        count = 0;
+        top--;
+      }
+    }
+    else{
+      count++;
+      stack[++top] = nums2[i];
+  }
+  }
+  return res;
+}
+
+int     main(void)
+{
+        int     nums1[] = {4, 2};
+        int     nums2[] = {1, 2, 3, 4};
+        int     *res;
+        int     len;
+        int     i;
+
+        i = -1;
+        len = sizeof(nums1) / sizeof(int);
+        res = ft_greater_elements(nums1, len, nums2, 4);
+        while (++i < len - 1)
+                printf(%d, , res[i]);
+        printf(%dn, res[i]);
+        free(res);
+        return (0);
+}
